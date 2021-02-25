67930b22c1006c077b12b8ee619d5681f730e5c9
ynawhocodes <ynawhocodes@gmail.com>
2021-02-25 17:44:43 +0900

    2.16

---
+++ Part01.The _Javascript _language/02.JavaScript_Fundamentals/2.16. Function expressions.js
@@ -0,0 +1,27 @@
+// 1번
+let ask = (question, yes, no) => {
+    if(confirm(question)) yes()
+    else no();
+}
+
+ask("동의하십니까", function() {alert("동의하셨습니다");}, function() {alert("취소 버튼을 누르셨습니다")});
+
+//2번
+function ask(question, yes, no) {
+    if(confirm(question)) yes()
+    else no();
+}
+
+ask("동의하십니까", () => alert("동의하셨습니다"), () => alert("취소 버튼을 누르셨습니다"));
+
+//3번
+let ask = (question, yes, no) => {
+    if(confirm(question)) yes()
+    else no();
+}
+
+ask("동의하십니까", () => alert("동의하셨습니다"), () => alert("취소 버튼을 누르셨습니다"));
+
+
+//4번
+let ask = () => {confirm("동의하십니까") ? () => alert("동의하셨습니다") : () => alert("취소 버튼을 누르셨습니다")}
